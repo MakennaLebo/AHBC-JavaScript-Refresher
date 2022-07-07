5ad769f5d28f1539ffb74269871d900f1ab59b3f
MakennaLebo <kennalebo@hotmail.com>
2022-07-06 22:22:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+{
+    const firstName = "Makenna";
+    const lastName= "Lebo";
+    const fullName= firstName + " " + lastName;
+    console.log (fullName);
+} 
+
+const population = "145492";
+console.log(population);
+
+let populationCount = population *3;
+console.log(populationCount);
+
+const javaScriptIsCool = true;
+
+const colors =["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+
+for ( let i = 1; i < 6; i++){
+console.log (i);
+}
+
+
+for ( let k = 0; k < 5; k++){
+console.log(k);
+}
